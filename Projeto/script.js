1498a2a71d4d5019fbe250f8a15ded2ce8c8a0fa
Lucas Bonfanti <lucasbonfanti97@gmail.com>
2022-08-06 18:18:25 -0300

    semana 3

--- Projeto/script.js
+++ Projeto/script.js
@@ -29,5 +29,44 @@ const media1 = 1139.5;
 const media2 = 1054.5;
 const media3 = 1220.5;
 
-const nacional = nacional1 && nacional2 && nacional3
+const nacionalMedia = nacional1 && nacional2 && nacional3
 console.log(nacional)
+
+console.log(`${nome1.toUpperCase()} \nAutor: ${autor1}\nAno de lançamento: ${lancamento1} \nNúmero de páginas: ${numeroPaginas1} \nGenero: ${genero1} \nÉ nacional? ${nacional1} \nPersonagens principais: ${personagensPrincipais1} `);
+console.log(`${nome2.toUpperCase()} \nAutor: ${autor2}\nAno de lançamento: ${lancamento2} \nNúmero de páginas: ${numeroPaginas2} \nGenero: ${genero2} \nÉ nacional? ${nacional2} \nPersonagens principais: ${personagensPrincipais2} `);
+console.log(`${nome3.toUpperCase()} \nAutor: ${autor3}\nAno de lançamento: ${lancamento3} \nNúmero de páginas: ${numeroPaginas3} \nGenero: ${genero3} \nÉ nacional? ${nacional3} \nPersonagens principais: ${personagensPrincipais3} `);
+
+const objeto1 = {
+    nome: 'Capitães de Areia',
+    autor: 'Jorge Amado',
+    lancamento: 1937,
+    numeroPaginas: 342,
+    genero: 'romance', 'ficcao',
+    nacional: true,
+    personagensPrincipais: 'Pedro Bala', 'Professor', 'Gato'
+}
+
+const objeto2 = {
+    nome: 'Dom Casmurro',
+    autor: 'Machado de Assis',
+    lancamento: 1899,
+    numeroPaginas: 210,
+    genero: 'suspense', 'ficcao', 'ficcao política'
+    nacional: false,
+    personagensPrincipais: 'Bentinho', 'Capitu'
+}
+
+const objeto3 = {
+    nome: 'Ponto de Impacto',
+    autor: 'Dan Brown',
+    lancamento: 2001,
+    numeroPaginas: 440,
+    genero: 'romance', 'ficcao',
+    nacional: true,
+    personagensPrincipais: 'Rachel Sexton', 'Michael Tolland'
+}
+
+const array = []
+
+array.push(objeto1)
+
