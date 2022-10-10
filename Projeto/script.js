3a6b9bb42d6fee380696374284441450bfe253b6
Lucas Bonfanti <lucasbonfanti97@gmail.com>
2022-10-10 17:04:16 -0300

    entrega projeto

--- Projeto/script.js
+++ Projeto/script.js
@@ -6,7 +6,6 @@
 // const nacional1 = true;
 // const personagensPrincipais1 = ['Pedro Bala', 'Professor', 'Gato'];
 
-
 // const nome2 = 'Dom Casmurro';
 // const autor2 = 'Machado de Assis';
 // const lancamento2 = 1899;
@@ -37,40 +36,127 @@
 // console.log(`${nome3.toUpperCase()} \nAutor: ${autor3}\nAno de lançamento: ${lancamento3} \nNúmero de páginas: ${numeroPaginas3} \nGenero: ${genero3} \nÉ nacional? ${nacional3} \nPersonagens principais: ${personagensPrincipais3} `);
 
 objeto1 = {
-    nome: 'Capitães de Areia',
-    autor: 'Jorge Amado',
-    lancamento: 1937,
-    numeroPaginas: 342,
-    genero: ['romance', 'ficcao'],
-    nacional: true,
-    personagensPrincipais: ['Pedro Bala', 'Professor', 'Gato']
-}
+  nome: "CAPITÃES DA AREIA",
+  autor: "Jorge Amado",
+  lancamento: 1937,
+  numeroPaginas: 342,
+  genero: ["romance", "ficcao"],
+  nacional: true,
+  personagensPrincipais: ["Pedro Bala", "Professor", "Gato"],
+  imagem:
+    "https://d2xsuil29zvzbt.cloudfront.net/imagens/img_m/812/239656_1.jpg",
+};
 
 objeto2 = {
-    nome: 'Dom Casmurro',
-    autor: 'Machado de Assis',
-    lancamento: 1899,
-    numeroPaginas: 210,
-    genero: ['suspense', 'ficcao', 'ficcao politica'],
-    nacional: false,
-    personagensPrincipais: ['Bentinho', 'Capitu']
-}
+  nome: "DOM CASMURRO",
+  autor: "Machado de Assis",
+  lancamento: 1899,
+  numeroPaginas: 210,
+  genero: ["suspense", "ficcao", "ficcao politica"],
+  nacional: true,
+  personagensPrincipais: ["Bentinho", "Capitu"],
+  imagem:
+    "https://images.tcdn.com.br/img/img_prod/783226/dom_casmurro_1115_1_20200630174117.png",
+};
 
 objeto3 = {
-    nome: 'Ponto de Impacto',
-    autor: 'Dan Brown',
-    lancamento: 2001,
-    numeroPaginas: 440,
-    genero: ['romance', 'ficcao'],
-    nacional: false,
-    personagensPrincipais: ['Rachel Sexton', 'Michael Tolland']
+  nome: "PONTO DE IMPACTO",
+  autor: "Dan Brown",
+  lancamento: 2001,
+  numeroPaginas: 440,
+  genero: ["romance", "ficcao"],
+  nacional: false,
+  personagensPrincipais: ["Rachel Sexton", "Michael Tolland"],
+  imagem: "https://images-na.ssl-images-amazon.com/images/I/71hRj2Y+PrL.jpg",
+};
+
+const listaDeLivros = [];
+
+if (objeto1.nacional === true) {
+  listaDeLivros.push(objeto1);
+} else {
+  alert("O item não adicionado");
 }
 
-const array = []
+if (objeto2.nacional === true) {
+  listaDeLivros.push(objeto2);
+} else {
+  alert("O item não foi adicionado");
+}
 
-array.push(objeto1)
-array.push(objeto2);
-array.push(objeto3);
+// if(objeto3.nacional === true){
+//     livros.push(objeto3)
+// }else{
+//     alert('O item não foi adicionado')
+// }
+// console.log(livros)
 
-console.log(array)
+listaDeLivros.push(objeto3);
 
+for (let i = 0; i < listaDeLivros.length; i++) {
+  console.log(
+    `Nome: ${listaDeLivros[i].nome.toLocaleUpperCase()}\nAutor: ${
+      listaDeLivros[i].autor
+    }\nLançamento: ${listaDeLivros[i].lancamento}\nNúmeros de Páginas: ${
+      listaDeLivros[i].numeroPaginas
+    }\nGênero: ${listaDeLivros[i].genero.join()}\nNacional: ${
+      listaDeLivros[i].nacional
+    }\nPersonagens: ${listaDeLivros[i].personagensPrincipais.join()}`
+  );
+}
+
+let relatorio = (objeto) => {
+  let stringObjeto = JSON.stringify(objeto);
+  console.log(stringObjeto);
+};
+
+relatorio(objeto1);
+relatorio(objeto2);
+relatorio(objeto3);
+
+let buscaLivro = (arrayObjeto, string) => {
+  let livro = [];
+  for (let i of arrayObjeto) {
+    if (i.nome.toUpperCase() === string.toUpperCase().trim()) {
+      livro.push(i);
+    }
+  }
+
+  if (livro.length !== 0) {
+    return livro;
+  } else {
+    alert("LIVRO NÃO ENCONTRADO");
+  }
+};
+
+const input = document.getElementById("txtBusca");
+
+
+const pesquisar = (event) => {
+  const livros = (listaDeLivros, nomeLivro) => {
+    let guardarLivros = [];
+    for (let i of listaDeLivros) {
+      const nomeLivro = input.value.toUpperCase();
+      if (i.nome === nomeLivro) {
+        guardarLivros.push(i);
+        txtBusca.value = "";
+        i.nome === "CAPITÃES DA AREIA"
+          ? capitaes.setAttribute("class", "section")
+          : capitaes.setAttribute("class", "none");
+        i.nome === "DOM CASMURRO"
+          ? dom.setAttribute("class", "section")
+          : dom.setAttribute("class", "none");
+        i.nome === "PONTO DE IMPACTO"
+          ? ponto.setAttribute("class", "section")
+          : ponto.setAttribute("class", "none");
+      }
+    }
+    if (guardarLivros.length === 0) {
+      txtBusca.value = "";
+      return alert("LIVRO NÃO ENCONTRADO");
+    } else {
+      return guardarLivros;
+    }
+  };
+  console.log(livros(listaDeLivros, "()"));
+};
